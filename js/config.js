de9707dc15d7d7a6970b3d81502e59c4d5528b72
Gholamreza.Rezaei <ghrezaei1399@gmail.com>
2026-08-08 12:40:41 +0330

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 // js/config.js
-const CONFIG = {
-    OPENAI_API_KEY: 'sk-...' // کلید واقعی خود را اینجا بگذارید
+window.CONFIG = {
+    OPENAI_API_KEY: 'YOUR_API_KEY' // کلید واقعی خود را اینجا بگذارید
 };
