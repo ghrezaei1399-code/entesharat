d9767aa0da9291bfb00e3b0d8faf0c045b7df482
Gholamreza.Rezaei <ghrezaei1399@gmail.com>
2026-08-08 13:05:18 +0330

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-// js/app.js
-document.addEventListener('DOMContentLoaded', () => {
-    const container = document.querySelector('.container');
-    if (container) {
-        // هدر و منو را در ابتدای container قرار بده
-        container.insertAdjacentHTML('afterbegin', Header.render());
-        container.insertAdjacentHTML('afterbegin', Menu.render());
-    }
-});
